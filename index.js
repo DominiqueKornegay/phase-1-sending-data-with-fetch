df89945b29230f7b48cdc61eb1c46ab209a0069c
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-06-22 21:41:38 -0400

    commit tree 636669813a6d0717a42a16c15767f3c34778f71b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const { Response } = require("node-fetch")
+
+  function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email : userEmail
+        })
+    })
+    .then((resp) => resp.json())
+    .then((data) => newContact(data.id))
+    .catch((response) => {
+        document.body.innerHTML = response
+        document.body.append()
+    })
+  }
+function newContact(userID) {
+   document.getElementsByTagName("body")[0].innerHTML = userID
+   document.body.append()
+  }
+
+submitData()
